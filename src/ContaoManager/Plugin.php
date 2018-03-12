23d648463b2d9c9b170fa67d9879188769408bf9
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-03-12 14:28:13 +0100

    added bundle classes and moved docs folders

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @author  Thomas Körner <t.koerner@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+
+namespace HeimrichHannot\FieldpaletteBundle\ContaoManager;
+
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use HeimrichHannot\FieldPalette\HeimrichHannotContaoFieldpaletteBundle;
+
+class Plugin implements BundlePluginInterface
+{
+
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @param ParserInterface $parser
+     *
+     * @return ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(HeimrichHannotContaoFieldpaletteBundle::class)->setLoadAfter([
+                ContaoCoreBundle::class
+            ])
+        ];
+    }
+}
---
+++ src/HeimrichHannotContaoFieldpaletteBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @author  Thomas Körner <t.koerner@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+
+namespace HeimrichHannot\FieldPalette;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HeimrichHannotContaoFieldpaletteBundle extends Bundle
+{
+
+}
