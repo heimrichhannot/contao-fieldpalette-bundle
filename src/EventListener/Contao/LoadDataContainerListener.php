c66a6cd335f456b9860030d9ccd24380c0115739
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-04-25 09:22:45 +0200

    latest

--- src/EventListener/Contao/LoadDataContainerListener.php
+++ src/EventListener/Contao/LoadDataContainerListener.php
@@ -64,7 +64,7 @@ class LoadDataContainerListener
 
         if ($this->registry->isFullyLoaded() && $changes) {
             $this->registry->refresh();
-            storeResults();
+            $this->registry->storeResults();
         }
     }
 }
