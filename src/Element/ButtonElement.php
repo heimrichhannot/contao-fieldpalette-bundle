41d1519f9d19125b380e8b1f0365d63472332efd
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-04-02 12:01:35 +0200

    fix dependency injection

--- src/Element/ButtonElement.php
+++ src/Element/ButtonElement.php
@@ -16,42 +16,28 @@ use HeimrichHannot\FieldpaletteBundle\Model\FieldPaletteModel;
 use HeimrichHannot\UtilsBundle\Util\Utils;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
-use Twig\Environment;
+use Twig\Environment as TwigEnvironment;
 
 class ButtonElement
 {
-    /**
-     * @var Environment
-     */
-    protected $twig;
+    protected array $options = [];
+    protected string $defaultTable;
+    protected ContaoFramework $framework;
+    protected TwigEnvironment $twig;
+    protected Utils $utils;
+    protected DcaHandler $dcaHandler;
 
-    /**
-     * @var array
-     */
-    protected $options = [];
-    /**
-     * @var ContaoFramework
-     */
-    private $framework;
-    /**
-     * @var string
-     */
-    private $defaultTable;
-    /**
-     * @var RoutingUtil
-     */
-    private $routeUtil;
-    /**
-     * @var DcaHandler
-     */
-    private $dcaHandler;
-
-    public function __construct(ContaoFramework $framework, string $table, Environment $twig, Utils $utils, DcaHandler $dcaHandler)
-    {
+    public function __construct(
+        string          $table,
+        ContaoFramework $framework,
+        TwigEnvironment $twig,
+        Utils           $utils,
+        DcaHandler      $dcaHandler
+    ) {
         $this->framework = $framework;
         $this->defaultTable = $table;
         $this->twig = $twig;
-        $this->routeUtil = $utils->routing();
+        $this->utils = $utils;
         $this->dcaHandler = $dcaHandler;
     }
 
@@ -123,7 +109,7 @@ class ButtonElement
         );
     }
 
-    public function setType($strType)
+    public function setType($strType): void
     {
         $this->act = $strType;
         $this->cssClass = $strType;
@@ -135,54 +121,54 @@ class ButtonElement
         }
     }
 
-    public function setModalTitle($varValue)
+    public function setModalTitle($varValue): static
     {
         $this->options['modalTitle'] = $varValue;
 
         return $this;
     }
 
-    public function setHref($varValue)
+    public function setHref($varValue): static
     {
         $this->options['href'] = $varValue;
 
         return $this;
     }
 
-    public function setTitle($varValue)
+    public function setTitle($varValue): static
     {
         $this->options['title'] = $varValue;
 
         return $this;
     }
 
-    public function setLabel($varValue)
+    public function setLabel($varValue): static
     {
         $this->options['label'] = $varValue;
 
         return $this;
     }
 
-    public function setId($varValue)
+    public function setId($varValue): static
     {
         $this->options['id'] = $varValue;
 
         return $this;
     }
 
-    public function getOptions()
+    public function getOptions(): array
     {
         return $this->options;
     }
 
-    public function addOptions(array $arrOptions = [])
+    public function addOptions(array $arrOptions = []): static
     {
         $this->options = array_merge($this->options, $arrOptions);
 
         return $this;
     }
 
-    public function setAttributes(array $attributes)
+    public function setAttributes(array $attributes): static
     {
         $this->options['attributes'] = $attributes;
 
@@ -193,7 +179,7 @@ class ButtonElement
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function getHref()
+    public function getHref(): string
     {
         return $this->generateHref();
     }
@@ -203,7 +189,7 @@ class ButtonElement
      * @throws NotFoundExceptionInterface
      * @throws Exception
      */
-    protected function generateHref()
+    protected function generateHref(): string
     {
         $parameter = $this->prepareParameter($this->act);
 
@@ -225,7 +211,7 @@ class ButtonElement
         // required by DC_TABLE::getNewPosition() within nested fieldpalettes
         $parameter['mode'] = 2;
 
-        return $this->routeUtil->generateBackendRoute($parameter, true);
+        return $this->utils->routing()->generateBackendRoute($parameter, true);
     }
 
     /**
